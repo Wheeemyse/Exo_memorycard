79eed74dda75431ba30bfecb7c125688936d806b
rhudra <sha08dow@hotmail.fr>
2018-06-15 00:01:05 +0200

    memory en cours

--- jordan/fonction.js
+++ jordan/fonction.js
@@ -23,34 +23,67 @@ function startTimer(){
 }
 
 //memory
-//on recupere les cartes sous forme d'un tableau.
-var card = document.getElementsByClassName('card');
-var cards = [...card];
-console.log(cards);
+var motifsCartes=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+var etatsCartes=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var cartesRetournees=[];
+var nbPairesTrouvees=0;
+var imgCartes=document.getElementById("cardDeck").getElementsByTagName("img");
+for(var i=0;i<imgCartes.length;i++){
+	imgCartes[i].noCarte=i; //Ajout de la propriété noCarte à l'objet img
+	imgCartes[i].onclick=function(){
+		controleJeu(this.noCarte);
+	}
+}
+function majAffichage(noCarte){
+	switch(etatsCartes[noCarte]){
+		case 0:
+			imgCartes[noCarte].src="../img/dos.jpg";
+			break;
+		case 1:
+			imgCartes[noCarte].src="../img/carte"+motifsCartes[noCarte]+".png";
+			break;
+		case -1:
+			imgCartes[noCarte].style.border="5px solid green";
+			break;
+	}
+}
+function rejouer(){
+	alert("Bravo !");
+	location.reload();
+}
+function initialiseJeu(){
+	for(var position=motifsCartes.length-1; position>=1; position--){
+		var hasard=Math.floor(Math.random()*(position+1));
+		var sauve=motifsCartes[position];
+		motifsCartes[position]=motifsCartes[hasard];
+		motifsCartes[hasard]=sauve;
+	}
+}
 
-//melange
-function randomImg(){
-   var tabImg = new Array();
-   tabImg[0]  = "../img/ajax.png";
-   tabImg[1]  = "../img/ajax.png";
-   tabImg[2]  = "../img/bootstrap.png";
-   tabImg[3]  = "../img/bootstrap.png";
-   tabImg[4]  = "../img/css.jpeg";
-   tabImg[5]  = "../img/css.jpeg";
-   tabImg[6]  = "../img/html.jpeg";
-   tabImg[7]  = "../img/html.jpeg";
-   tabImg[8]  = "../img/jquery.png";
-   tabImg[9]  = "../img/jquery.png";
-   tabImg[10]  = "../img/js.png";
-   tabImg[11]  = "../img/js.png";
-   tabImg[12]  = "../img/node js.jpeg";
-   tabImg[13]  = "../img/node js.jpeg";
-   tabImg[14]  = "../img/php.png";
-   tabImg[15]  = "../img/php.png";
-   tabImg[16]  = "../img/python.png";
-   tabImg[17]  = "../img/python.png";
-   tabImg[18]  = "../img/ruby.png";
-   tabImg[19]  = "../img/ruby.png";
+function controleJeu(noCarte){
+  if(cartesRetournees.length<2){
+    if(etatsCartes[noCarte]==0){
+			etatsCartes[noCarte]=1;
+			cartesRetournees.push(noCarte);
+			majAffichage(noCarte);
+		}
+    if(cartesRetournees.length==2){
+			var nouveauEtat=0;
+			if(motifsCartes[cartesRetournees[0]]==motifsCartes[cartesRetournees[1]]){
+				nouveauEtat=-1;
+				nbPairesTrouvees++;
+			}
 
-   card.src += tabImg[Math.round(Math.random()*21)];
+			etatsCartes[cartesRetournees[0]]=nouveauEtat;
+			etatsCartes[cartesRetournees[1]]=nouveauEtat;
+      setTimeout(function(){
+    majAffichage(cartesRetournees[0]);
+    majAffichage(cartesRetournees[1]);
+    cartesRetournees=[];
+    if(nbPairesTrouvees==10){
+      rejouer();
+    }
+  },750);
+}
+}
 }
